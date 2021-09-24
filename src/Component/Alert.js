71b062c442c2f297c27d4cee19866de7f3bb599b
Debdutta0507 <70414511+Debdutta0507@users.noreply.github.com>
2021-09-24 15:47:31 +0530

    Weather-app

--- src/Component/Alert.js
+++ src/Component/Alert.js
@@ -8,12 +8,14 @@ export default function Alert(props) {
 
     }
     return (
-        props.alert && <div>
+        <div style={{height: '60px'}}>
+        {props.alert && <div>
             <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                <strong> {capitalize(props.alert.type)} {props.alert.msg}</strong>
                
             </div>
 
+        </div>}
         </div>
     )
 }
---
+++ src/Component/Weather.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+
+
+export default function Weather() {
+    const [repo, setRepo] = useState([]);
+
+    const getRepo = () => axios.get('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0 ')
+        .then(response => {
+            console.log(response.data.dataseries);
+            const myRepo = response.data.dataseries;
+            setRepo(myRepo)
+
+        });
+    useEffect(() => getRepo(), []);
+    return (
+        <div>
+            {/*  {repo.map((repos) => (
+                <><p>{repos.timepoint}</p>
+                <p>{repos.cloudcover}</p>
+                <p>{repos.prec_type}</p>
+                <p>{repos.temp2m}</p>
+                <p>{repos.wind10m.speed}</p>
+                <p>{repos.seeing}</p></>
+                <th>#</th>
+                        <th>Time</th>
+                        <th>Cloud</th>
+                        <th>Precipitation</th>
+                        <th>Temperature</th>
+                        <th>WindSpeed</th>
+                        <th>WindDirection</th>
+
+            ))
+            } */}
+            {
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Time</th>
+                            <th>Cloud</th>
+                            <th>Precipitation</th>
+                            <th>Temperature</th>
+                            <th>WindSpeed</th>
+                            <th>WindDirection</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                            {repo.map((repos) => (
+                               <> <tr>
+                             <td>{repos.timepoint}</td>
+                                <td>{repos.cloudcover}</td>
+                                <td>{repos.prec_type}</td>
+                                <td>{repos.temp2m}</td>
+                                <td>{repos.wind10m.speed}</td>
+                                <td>{repos.wind10m.direction}</td>
+                                </tr></>
+                            ))
+                            }
+
+
+                        
+                    </tbody>
+
+
+
+                </Table>
+            }
+
+        </div>
+
+    )
+}
+
